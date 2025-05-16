c07741862d25723b2e31816259077f7f813def81
Antonio-Felix <antonio980felixmedeiros@gmail.com>
2025-05-16 10:01:43 -0300

    update

--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Filme
 
 def lista_filmes(request):
-    filmes = Filme.objects.all()
+    lista = Filme.objects.all().order_by('-id')
+    paginator = Paginator(lista, 3)
+
+    pagina = request.GET.get('page')
+    filmes = paginator.get_page(pagina)
+
     return render(request, 'filmes/lista.html', {'filmes': filmes})
